c0b47dea1e9374749d683733d533000cee48f20c
Gilvari <118232462+Gilvari@users.noreply.github.com>
2022-11-16 01:00:16 -0500

    Add files via upload

---
+++ problem-ctrldocnature.sql
@@ -0,0 +1,97 @@
+﻿
+EXEC USP_ACC_SAVE_CtrlDocNature @DId =1608963
+/*
+
+*/
+
+EXEC USP_ACC_SAVE_CtrlDocNature @DId =1608971
+/*
+ */---------------------------------------------select  * from acc_accdoc dinner join acc_accdocrow dr on dr.did=d.idwhere did in (1608963,1608971)/**/
+
+-------
+
+
+SELECT
+		t3.Code N'کد مرکز',
+		t3.Name N'نام مرکز',
+		CASE t3.Independent
+			WHEN 1 THEN N'مستقل'
+			ELSE N'غیرمستقل'
+			END N'وضعیت مرکز',
+		d.Year AS N'سال مالی',
+		CAST(dr.DId AS NVARCHAR(MAX)) AS N'شناسه سند',
+		dr.RowNo AS N'شماره ردیف سند',
+
+       CASE d.DocType
+			WHEN 0 THEN N'افتتاحيه حسابها'
+			WHEN 1 THEN N'اختتاميه'
+			WHEN 2 THEN N'سود و زيان'
+			WHEN 3 THEN N'عادي'
+			WHEN 4 THEN N'اصلاحي'
+			WHEN 5 THEN N'سيستم اموال'
+			WHEN 6 THEN N'سيستم كالا'
+			WHEN 7 THEN N'سيستم بودجه و اعتبارات'
+			WHEN 8 THEN N'سيستم دريافت پرداخت'
+			WHEN 9 THEN N'سيستم حقوق و دستمزد'
+			WHEN 10 THEN N'ايجاد سوابق اموال'
+			WHEN 11 THEN N'ايجاد سوابق كالا'
+			WHEN 12 THEN N'ارسالي از واحد'
+			WHEN 13 THEN N'سيستم تجميع اسناد'
+			END AS N'نوع سند',
+			
+		CASE d.state
+	        
+			WHEN 1 THEN N'در انتظار رسيدگی'
+			WHEN 2 THEN N'واخواهی شده یا همان برگشت از رسیدگی'
+			WHEN 3 THEN N'آماده تامین اعتبار'
+			WHEN 4 THEN N'آماده ثبت عمليات دریافت - پرداخت'
+			WHEN 5 THEN N'آماده رسیدگی نهایی'
+			WHEN 6 THEN N'آماده تایيد وصول مستندات'
+			WHEN 7 THEN N'آماده ارسال'
+			WHEN 8 THEN N'نهایی'
+			WHEN 9 THEN N'آماده تایيد مقام مجاز جهت برداشت وجه'
+			WHEN 10 THEN N'آماده تایيد مقام مجاز جهت ثبت'
+			WHEN 11 THEN N'آماده شناسایی کد اعتبار'
+			WHEN NULL THEN N'بدون سند'
+		    ELSE ''
+            END AS N'وضعیت سند',
+
+         CAST(dr.Id AS NVARCHAR(MAX)) AS N'شناسه ردیف سند',
+         CASE WHEN CAST(c.Code AS NVARCHAR(MAX)) IS NULL THEN N'ندارد' ELSE CAST(c.Code AS NVARCHAR(MAX)) END AS N'کد حساب',
+         c.Name N'نام حساب',
+         CASE WHEN c2.HT1 = 0 THEN N'خیر' ELSE N'بله' END AS N'تفصیل یک نیاز دارد',
+         dr.T1Id N'شناسه تفصیل یک',
+         CASE WHEN c2.HT2 = 0 THEN N'خیر' ELSE N'بله' END AS N'تفصیل دو نیاز دارد',
+         dr.T2Id AS N'شناسه تفصیل دو',
+         CASE WHEN c2.HT3 = 0 THEN N'خیر' ELSE N'بله' END AS N'تفصیل سه نیاز دارد',
+         dr.T3Id AS N'شناسه تفصیل سه',
+         CASE WHEN c2.Hproj = 0 THEN N'خیر' ELSE N'بله' END AS N'پروژه نیاز دارد', 
+         ProjectId AS N'شناسه پروژه',
+         CASE WHEN c2.HasCheck = 0 THEN N'خیر' ELSE N'بله' END AS N'چک نیاز دارد', 
+         dr.CheckId AS N'شناسه چک',
+         dr.ChekNo AS N'شماره چک',
+         dr.Document AS N'شماره فیش',
+         CASE WHEN c2.HCC = 0 THEN N'خیر' ELSE N'بله' END AS N'مرکز هزینه',
+         CostCenterId AS N'شناسه مرکز هزینه',
+         UnitId AS N'واحد سازمانی'
+
+FROM dbo.ACC_AccDocRow AS Dr
+LEFT OUTER JOIN dbo.M_ACC_Codes2 AS c2 ON dr.CodeId = c2.MId 					
+LEFT OUTER JOIN dbo.M_ACC_Codes As c ON c2.MId = c.Id
+INNER JOIN ACC_AccDoc d ON dr.DId = d.Id
+INNER JOIN M_ORG_T3 t3 ON t3.Id = d.T3Id
+WHERE (
+		 
+			(c2.HT1 = 0 AND dr.T1Id IS not NULL)
+		OR
+			(c2.HCC = 0) AND (dr.CostCenterId IS not NULL or dr.UnitId IS not NULL)
+		OR
+			(c2.HT3 = 0 AND dr.T3Id IS not NULL)
+		OR
+			(c2.HProj = 0 AND dr.ProjectId IS not NULL)
+        OR
+			(c2.HT2 = 0 AND dr.T2Id IS not NULL)
+		OR
+			(c2.HasCheck = 0) AND (dr.CheckId IS not NULL or dr.ChekNo IS not NULL or dr.Document IS not NULL)
+		)
+		AND d.Id in (1608963,1608971)
---
+++ removecardid.sql
@@ -0,0 +1,72 @@
+﻿
+Declare @STR NVARCHAR(MAX),@STRPARSMS NVARCHAR(MAX) =N' @cardid int'
+set @STR='
+declare @Commit bit set @Commit=0
+select typeindex into #card 
+from AST_History
+where CardId=@cardid
+
+declare @strMessage1 nvarchar(100)=''''
+declare @strMessage2 nvarchar(100)=''''
+DECLARE @AMORTIZATION BIGINT=0
+
+
+if (select count(cardid) from AST_History where CardId=@cardid)=0
+Begin
+	set @strMessage1=''کارت اموال با شناسه''+CONVERT(nvarchar(100),@cardid)+'' وجود ندارد''
+	RaisError(@strMessage1,16,1)
+End
+else
+	   SET @AMORTIZATION=(select SUM(Amortization) from AST_StoredHistory where CardId=@cardid)
+	   
+if (select COUNT(typeindex) from #card where typeindex<>0 and typeindex<>1 and typeindex<>8)!=0
+     OR (@AMORTIZATION>0)
+     
+	Begin 
+		set @strMessage2=''امکان پاک کردن کارت اموال با شناسه ''+CONVERT(nvarchar(100),@cardid)+'' وجود ندارد''
+		RaisError(@strMessage2,16,1)
+	end
+else
+Begin
+	begin try
+		Begin tran A
+		
+		 if exists (select cardid from AST_HolderExpiredHistory where CardId=@cardid)
+			Begin
+				delete from AST_HolderExpiredHistory
+				where CardId=@cardid
+			End
+			
+			delete from ast_storedhistory
+			where CardId=@cardid
+
+			delete from AST_History
+			where CardId=@cardid
+
+			delete from AST_Card
+			where id=@cardid
+
+ IF (@Commit = 1)
+    BEGIN
+        PRINT ''کارت اموال با شناسه''+CONVERT(nvarchar(100),@cardid)+''پاک شد''
+        COMMIT TRAN A
+    END
+    ELSE
+    BEGIN
+        PRINT ''ID: ''+CONVERT(nvarchar(100),@cardid)+'' Removed in Rollback''
+                ROLLBACK TRAN A
+    END
+END TRY
+BEGIN CATCH
+    Declare @Message VARCHAR(MAX)
+    Set @Message = ERROR_MESSAGE()
+    ROLLBACK TRAN A
+    Exec usp_None_Load_RAISERROR @Message
+END CATCH
+End
+drop table #card
+'
+exec sp_executesql @STR,@STRPARSMS ,@cardid=404
+
+
+
